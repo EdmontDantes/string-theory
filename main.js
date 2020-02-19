0cda94ba06447d45a516e3fe92e4c2d6c76ef56a
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-02-19 11:53:34 -0500

    half finished

--- main.js
+++ main.js
@@ -1,22 +1,104 @@
-function crazyCase() {
+function crazyCase(str) {
+  let answer = '';
+  for (let i = 0; i < str.length; i++){
+      if (i % 2 === 0) {
+      answer = answer + str[i].toLowerCase();
+    }   else if (i % 2 === 1) {
+        answer = answer + str[i].toUpperCase();
+    }
+  }
+  return answer;
 }
 
-function ciEmailify() {
+crazyCase('Hello')
+
+function ciEmailify(str) {
+  let answer = '';
+  for (let i = 0 ; i < str.length; i++){
+    if (str[i] === ' ') { 
+      answer = answer + '.';
+    } else {
+      answer = answer + str[i]; 
+    }
+  }
+return answer.toLowerCase() + '@codeimmersives.com';
 }
 
-function exclaim() {
+
+
+
+
+function exclaim(str) {
+  let answer = '';
+  for (let i = 0 ; i < str.length; i++){
+    if (str[i] === '?' || str[i] === '.') { 
+      answer = answer + '!';
+    } else {
+      answer = answer + str[i]; 
+    }
+  }
+return answer;
 }
 
-function reverse() {
+
+
+// function exclaim('I am the best');
+
+function reverse(str){
+  let reversed = "";    
+  for (let i = str.length - 1; i >= 0; i--){        
+    reversed += str[i];
+  }    
+  return reversed;
 }
 
-function crazyCase2ReturnOfCrazyCase() {
+function crazyCase2ReturnOfCrazyCase(str) {
+  let answer  = '';
+for (i = 0; i < str.length; i++) {
+  if (i % 2 === 0 && str[i] !== ' ') {
+
+  answer = answer + str[i].toLowerCase();
+
+  } else if (i % 2 === 1 && str[i] === ' ') {
+    answer = answer + str[i].toUpperCase();
+
+  } else if (i % 2 === 0) {
+
+  answer = answer + str[i].toLowerCase();
+
+  } else if (i % 2 === 1) {
+    
+    answer = answer + str[i].toUpperCase();
+  }
 }
 
-function titleCase() {
+  return answer;
 }
 
-function onlyVowels() {
+function titleCase(str) {  
+  let answer = '';
+
+for (let i=0; i<str.length; i++){
+  if(i = 0) {
+    str.toUpperCase();
+  } else if ()
+    output += ch.toLowerCase()
+  }else{
+    output += ch.toUpperCase()
+  }
+}
+return output
+}
+
+
+function onlyVowels(str) {
+  let answer = ''
+  for (i = 0; i < str.length; i++) {
+    if (('aeoiuAEOIU'.includes(str.charAt(i))) === true) {
+      answer = answer + str[i];
+    }
+  } 
+  return answer;
 }
 
 function crazyCase3SonOfCrazyCase() {
